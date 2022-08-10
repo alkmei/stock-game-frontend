89655443eb90090a78c858c1be375677d3976cf3
Alvin Mei <alvinkmei@gmail.com>
2022-08-10 12:45:27 -0400

    Update InfoBox.vue

--- src/components/GamePage/InfoBox.vue
+++ src/components/GamePage/InfoBox.vue
@@ -1,16 +1,60 @@
 <template>
-  <div class="info-box"></div>
+  <div class="info-box">
+
+
+    <table class="fixed_header">
+      <thead>
+      <tr>
+        <th>Ranking</th>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Money</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="n in 9">
+        <td>row {{ n }}-0</td>
+        <td>row {{ n }}-1</td>
+        <td>row {{ n }}-2</td>
+        <td>row {{ n }}-3</td>
+      </tr>
+      </tbody>
+    </table>
+
+
+  </div>
 </template>
 
-<script>
-export default {
-  name: "InfoBox"
-}
+<script setup>
 </script>
 
 <style scoped lang="scss">
 .info-box {
-  @include box;
-
+  height: 100%;
+  border: 1px solid;
+  .fixed_header {
+    width: 100%;
+    table-layout: fixed;
+    border-collapse: collapse;
+    tbody {
+      display:block;
+      width: 100%;
+      overflow: auto;
+      height: 100%;
+    }
+    thead {
+      background: black;
+      color:#fff;
+      tr {
+        display: block;
+      }
+    }
+    th, td {
+      padding: 5px;
+      text-align: center;
+      width: 20rem;
+      font-size: 1.5rem;
+    }
+  }
 }
 </style>
