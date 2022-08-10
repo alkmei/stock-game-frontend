afe7c5ec85f9e07ffdef16867da59a8620f53d86
Alvin <alvinkmei@gmail.com>
2022-08-10 01:05:32 -0400

    hjk

--- src/components/GamePage/StockList.vue
+++ src/components/GamePage/StockList.vue
@@ -1,30 +1,17 @@
 <template>
 <div class="stock-list-window">
   <div class="stock-list-wrapper">
-    <table class="stock-list" v-if="store.stocks.length > 0">
-      <tr>
-        <th class="ticker title">Ticker</th>
-        <th class="name title">Name</th>
-        <th class="price title">Price</th>
-        <th class="price title">Top Today</th>
-        <th class="price title right">Bottom Today</th>
-      </tr>
-      <tr class="stock" v-for="stock in store.stocks">
-        <th class="ticker">{{ stock.ticker }}</th>
-        <th class="name">{{ stock.name }}</th>
-        <th class="price">
-          ${{ stock.price.toString().split(".")[0] }}.{{stock.price.toString().split(".")[1].padEnd(2, "0")}}
-        </th>
-        <th class="price">
-          ${{ stock.today_max.toString().split(".")[0] }}.{{stock.today_max.toString().split(".")[1].padEnd(2, "0")}}
-        </th>
-        <th class="price right">
-          ${{ stock.today_min.toString().split(".")[0] }}.{{stock.today_min.toString().split(".")[1].padEnd(2, "0")}}
-        </th>
-      </tr>
-    </table>
+    <div class="top-header">
+      <div class="top-header-row">
+        <span>Name</span>
+        <span>Ticker</span>
+        <span>Price</span>
+        <span>Highest Today</span>
+        <span>Lowest Today</span>
+      </div>
+    </div>
   </div>
-  <div class="stock-display"></div>
+  <div class="stock-info-wrapper"></div>
 </div>
 </template>
 
@@ -39,5 +26,24 @@ store.getStocks()
   border-top: 1px solid;
   padding: 1rem;
   height: 25rem;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 1rem;
+  .stock-list-wrapper {
+    @include box;
+    display: table;
+    .top-row {
+      display: table-header-group;
+      .top-header-row {
+        display: table-row;
+        span {
+          display: table-cell;
+        }
+      }
+    }
+  }
+  .stock-info-wrapper {
+    @include box
+  }
 }
 </style>
